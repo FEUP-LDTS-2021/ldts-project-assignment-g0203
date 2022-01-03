f4d3cb50f8f0ac8438617c5116a7e331ba95435c
shini <raquelmcarneiro16@gmail.com>
2022-01-03 23:18:31 +0000

    To start, based the snake on hero.

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+// THE CLASS APPLICATION HAS THE PURPOSE TO START OUR SNAKE GAME!
+public class Application {
+    public static void main(String[] args) throws IOException{
+        Game game = new Game();
+        game.run();
+    }
+}
